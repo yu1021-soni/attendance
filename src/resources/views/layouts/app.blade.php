4e7cd0d038aaa03d2bcf863e19252ba80545b731
yu <yuukasonia1021@gmail.com>
2025-11-05 08:49:31 +0900

    register.blade

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="UTF-8">
@@ -11,3 +11,57 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </head>
+
+<body>
+    <div class="app">
+        <header class="header">
+            <div class="header__inner">
+                <a href="/" class="header__logo-link">
+                    <img src="{{ asset('img/logo.svg') }}" alt="COACHTECH" class="header__logo">
+                </a>
+
+                {{-- login,register, mailhog ページではロゴだけ --}}
+                @if (!Route::is('register') && !Route::is('mailhog') && !Route::is('login') && !Route::is('verification.*'))
+
+                <form class="header__search" action="/search" method="get">
+                    <input class="header__search-input" type="text" name="keyword" placeholder="なにをお探しですか？" value="{{ request('keyword') }}">
+                </form>
+
+                <nav class="header__nav">
+                    <ul class="header__menu">
+                        <li>
+                            @if(Auth::check())
+                            <form method="post" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="header__logout">ログアウト</button>
+                            </form>
+                            @else
+                            <form action="{{ route('login') }}" method="get">
+                                <button type="submit" class="header__login">ログイン</button>
+                            </form>
+                            @endif
+                        </li>
+                        <li>
+                            <form method="post" action="{{ route('mypage') }}">
+                                @csrf
+                                <button type="submit" class="header__mypage">マイページ</button>
+                            </form>
+                        </li>
+                        <li>
+                            <a href="{{ route('item.sell') }}" class="header__sell">出品</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                @endif
+            </div>
+        </header>
+
+        <div class="content">
+            @yield('content')
+        </div>
+
+    </div>
+</body>
+
+</html>
