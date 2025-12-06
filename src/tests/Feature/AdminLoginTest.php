19b173d2bb94ce49ae2aa37e2377b24b394c0f45
yu <yuukasonia1021@gmail.com>
2025-12-06 10:00:42 +0900

    AdminLoginTest

--- src/tests/Feature/AdminLoginTest.php
+++ src/tests/Feature/AdminLoginTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 
 class AdminLoginTest extends TestCase
@@ -16,5 +16,77 @@ class AdminLoginTest extends TestCase
      */
     use RefreshDatabase;
 
-    
+    public function test_admin_login_email_required()
+    {
+        //1. 管理者ユーザを登録する
+        User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password123'),
+            'role' => 1,
+        ]);
+
+        // ログインページを開く
+        $response = $this->from('/admin/login')
+            ->post('/admin/login', [
+                'email' => '',
+                'password' => 'password123',
+            ]);
+
+        // /admin/loginにリダイレクト
+        $response->assertRedirect('/admin/login');
+
+        // バリデーションメッセージ表示
+        $response->assertSessionHasErrors('email');
+    }
+
+
+
+    public function test_admin_login_password_required()
+    {
+        //1. 管理者ユーザを登録する
+        User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password123'),
+            'role' => 1,
+        ]);
+
+        // ログインページを開く
+        $response = $this->from('/admin/login')
+            ->post('/admin/login', [
+                'email' => 'admin@example.com',
+                'password' => '',
+            ]);
+
+        // /admin/loginにリダイレクト
+        $response->assertRedirect('/admin/login');
+
+        // バリデーションメッセージ表示
+        $response->assertSessionHasErrors('password');
+    }
+
+
+
+
+    public function test_admin_login_input_information_error()
+    {
+        //1. 管理者ユーザを登録する
+        User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password123'),
+            'role' => 1, // 管理者
+        ]);
+
+        // ログインページを開く
+        $response = $this->from('/admin/login')
+            ->post('/admin/login', [
+                'email' => 'wrong@example.com',
+                'password' => 'password123',
+            ]);
+
+        // /admin/loginにリダイレクト
+        $response->assertRedirect('/admin/login');
+
+        // バリデーションメッセージ表示
+        $response->assertSessionHasErrors('email');
+    }
 }
