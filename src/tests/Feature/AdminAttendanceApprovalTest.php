cd9623530dc422b95ebd48eb449843d201cf1a17
yu <yuukasonia1021@gmail.com>
2025-12-09 17:37:11 +0900

    テスト//準備

--- src/tests/Feature/AdminAttendanceApprovalTest.php
+++ src/tests/Feature/AdminAttendanceApprovalTest.php
@@ -13,10 +13,45 @@ class AdminAttendanceApprovalTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    // 承認待ちの修正申請が全て表示されている
+    public function test_pending_corrections_are_displayed() {
+
+        // 1. 管理者ユーザーにログインをする
+
+        // 2. 修正申請一覧ページを開き、承認待ちのタブを開く
+
+        // 全ユーザーの未承認の修正申請が表示される
+    }
+
+    // 承認済みの修正申請が全て表示されている
+    public function test_approved_corrections_are_displayed() {
+
+        // 1. 管理者ユーザーにログインをする
+
+        // 2. 修正申請一覧ページを開き、承認待ちのタブを開く
+
+        // 全ユーザーの承認済みの修正申請が表示される
+    }
+
+    // 修正申請の詳細内容が正しく表示されている
+    public function test_correction_detail_displays_correct_information() {
+
+        // 1. 管理者ユーザーにログインをする
+
+        // 2. 修正申請の詳細画面を開く
+
+        // 申請内容が正しく表示されている
+    }
+
+    // 修正申請の承認処理が正しく行われる
+    public function test_correction_approval_updates_database() {
+
+        // 1. 管理者ユーザーにログインをする
+
+        // 2. 修正申請の詳細画面で「承認」ボタンを押す
+
+        // 修正申請が承認され、勤怠情報が更新される
     }
 }
--- src/tests/Feature/AdminGetUserInfoTest.php
+++ src/tests/Feature/AdminGetUserInfoTest.php
@@ -13,10 +13,61 @@ class AdminGetUserInfoTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    // 管理者ユーザーが全一般ユーザーの「氏名」「メールアドレス」を確認できる
+    public function test_admin_can_view_all_general_users_with_name_and_email() {
+
+        // 1. 管理者でログインする
+
+        // 2. スタッフ一覧ページを開く
+
+        // 全ての一般ユーザーの氏名とメールアドレスが正しく表示されている
+    }
+
+    // ユーザーの勤怠情報が正しく表示される
+    public function test_attendance_records_are_displayed_correctly() {
+
+        // 1. 管理者ユーザーでログインする
+
+        // 2. 選択したユーザーの勤怠一覧ページを開く
+
+        // 勤怠情報が正確に表示される
+    }
+
+    // 「前月」を押下した時に表示月の前月の情報が表示される
+    public function test_previous_month_button_displays_previous_month_records() {
+
+        // 1. 管理者ユーザーにログインをする
+
+        // 2. 勤怠一覧ページを開く
+
+        // 3. 「前月」ボタンを押す
+
+        // 前月の情報が表示されている
+    }
+
+    // 「翌月」を押下した時に表示月の前月の情報が表示される
+    public function test_next_month_button_displays_next_month_records() {
+
+        // 1. 管理者ユーザーにログインをする
+
+        // 2. 勤怠一覧ページを開く
+
+        // 3. 「翌月」ボタンを押す
+
+        // 翌月の情報が表示されている
+    }
+
+    // 「詳細」を押下すると、その日の勤怠詳細画面に遷移する
+    public function test_detail_button_navigates_to_attendance_detail_page() {
+
+        // 1. 管理者ユーザーにログインをする
+
+        // 2. 勤怠一覧ページを開く
+
+        // 3. 「詳細」ボタンを押下する 
+
+        // その日の勤怠詳細画面に遷移する
     }
 }
